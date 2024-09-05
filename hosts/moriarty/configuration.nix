1df6b499ab9995a3ab0f9edcac72c00f1b2f60cc
MinimumAction <liguori.km@gmail.com>
2024-09-05 16:48:46 -0400

    new hosts

---
+++ hosts/moriarty/configuration.nix
@@ -0,0 +1,19 @@
+# moriarty configuration
+# live usb configuration
+
+{ config, lib, pkgs, modulesPath, ... }:{
+
+  # Imports
+  imports =[ 
+    "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix" # minimal NixOS install
+    ./../../modules/defaults.nix # default settings/programs
+  ];
+
+  # Drivers for external wifi adapter
+  boot.extraModulePackages = with config.boot.kernelPackages; [ rtl8812au ];
+  boot.initrd.kernelModules = [ "8812au" ];
+
+  # Live usb specific system packages
+  environment.systemPackages = with pkgs; [];
+
+}
