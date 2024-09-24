0b15f6f383cb1ee3019cb07d52b76bed12648ca4
MinimumAction <liguori.km@gmail.com>
2024-09-24 15:21:04 -0400

    added gui to watson

--- hosts/watson/configuration.nix
+++ hosts/watson/configuration.nix
@@ -15,6 +15,9 @@
   boot.extraModulePackages = with config.boot.kernelPackages; [ rtl8812au ];
   boot.initrd.kernelModules = [ "8812au" ];
 
+  # Turn off wireless
+  networking.wireless.enable = false;
+  
   # Live usb specific system packages
   environment.systemPackages = with pkgs; [];
 
